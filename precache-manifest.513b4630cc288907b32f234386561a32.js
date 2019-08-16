6f8833b0b0109f0fe687173ddf73a61ff489c8ee
Cameron Cozza <cameroncozza@gmail.com>
2019-08-16 10:41:12 -0700

    Updates

---
+++ precache-manifest.513b4630cc288907b32f234386561a32.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7784ef8cbe98a80883acad0fd20d4c9d",
+    "url": "/obligatory-todo-app-with-react/index.html"
+  },
+  {
+    "revision": "441118ad4b79e64e58f3",
+    "url": "/obligatory-todo-app-with-react/static/css/main.9ed85a18.chunk.css"
+  },
+  {
+    "revision": "07645a9b5fd1607fbae5",
+    "url": "/obligatory-todo-app-with-react/static/js/2.9faada80.chunk.js"
+  },
+  {
+    "revision": "441118ad4b79e64e58f3",
+    "url": "/obligatory-todo-app-with-react/static/js/main.b6372e3a.chunk.js"
+  },
+  {
+    "revision": "087ae4bb8145051a6f29",
+    "url": "/obligatory-todo-app-with-react/static/js/runtime~main.c1b51f38.js"
+  }
+]);
